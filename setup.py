8ddce23b6320621e4a17de54f66fa224183a28bd
Raverdy <piere-guillaume.raverdy@inria.fr>
2024-11-13 18:01:10 +0000

    Updated config

--- setup.py
+++ setup.py
@@ -21,5 +21,8 @@ setup(
  license='BSD-3-Clause',
 #  tests_require=['pytest'],
  entry_points={
+     'console_scripts': [
+             'go2_shutdown = go2_control_interface.sportsmode_shutdown:main'
+     ],
    },
 )
