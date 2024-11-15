e6db19b4cf9ba2519ccc3b508df9bc611b8c77c9
earlaud <etienne.arlaud@inria.fr>
2024-11-15 18:28:08 +0100

    Add arming/disarming watchdog

--- scripts/watchdog.py
+++ scripts/watchdog.py
@@ -5,6 +5,7 @@ from rclpy.node import Node
 from go2_control_interface.robot_interface import Go2RobotInterface
 import threading
 from unitree_go.msg import LowCmd
+from std_msgs.msg import Bool
 
 
 class WatchDogNode(Node, Go2RobotInterface):
@@ -16,22 +17,52 @@ class WatchDogNode(Node, Go2RobotInterface):
         self.freq = self.declare_parameter("freq", 100).value
         self.n_fail = self.declare_parameter("n_fail", 2).value
 
-        self.lowcmd_subscription =  self.create_subscription(LowCmd, "lowcmd", self.__cmd_cb, 10)
-        self.timer = self.create_timer(1./self.freq, self.timer_callback)
         self.cnt = 0
         self.is_stopped = False
+        self.is_waiting = False
+
+        self.lowcmd_subscription =  self.create_subscription(LowCmd, "lowcmd", self.__cmd_cb, 10)
+        self.start_subscription =  self.create_subscription(Bool, "/watchdog/start", self.__start_cb, 10)
+        self.timer = self.create_timer(1./self.freq, self.timer_callback)
+
+    def __start_cb(self, msg):
+        # Acting as an e-stop
+        if not msg.data:
+            self.is_stopped = True
+            return
+
+        # Arming the watchdog
+        self.is_waiting = True
+        self.is_stopped = False
+        self.get_logger().warning("Watch-dog ready, waiting for /lowcmd")
+
 
     def __cmd_cb(self, msg):
+        if self.is_waiting:
+            self.get_logger().warning("First command received on /lowcmd, watchdog armed")
+
+        self.is_waiting = False
         self.cnt = 0
 
+
     def timer_callback(self):
-        self.cnt += 1
-        if self.is_stopped or self.cnt >= self.n_fail:
+        # Does not count is the watchdog is armed and wait for the first command
+        if not self.is_waiting:
+            self.cnt += 1
+
+        # If the counter exceed, trigger stop
+        if self.cnt >= self.n_fail:
+            self._kill_robot() # ASAP
             if not self.is_stopped:
                 self.get_logger().error("Watch-dog reach sending damping to all joints, spamming")
             self.is_stopped = True
-            self._send_command([0.]*12, [0.]*12, [0.]*12, [0.]*12, [1.]*12, 1.0)
 
+        # If stopped send damping command
+        if self.is_stopped:
+            self._kill_robot()
+
+    def _kill_robot(self):
+        self._send_command([0.]*12, [0.]*12, [0.]*12, [0.]*12, [1.]*12, 1.0)
 
 def main(args=None):
     rclpy.init(args=args)
