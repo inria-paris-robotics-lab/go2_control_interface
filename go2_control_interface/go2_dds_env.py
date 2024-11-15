330d1a0b215ee6b7935372b99040a9b71f3a5eef
raverdy <pierre-guillaume.raverdy@inria.fr>
2024-11-15 13:21:56 +0100

    Splitted output for sourcing

--- go2_control_interface/go2_dds_env.py
+++ go2_control_interface/go2_dds_env.py
@@ -1,54 +1,58 @@
 import socket
 import fcntl
 import struct
-import os
+import os, sys
 from typing import Optional
 
 from go2_netwif import Go2NetworkInfo
 
-import importlib
-
+import importlib, pathlib
+import importlib.util
 
 class Go2EnvSetup():
-
-    ROOT_UNITREE = "/root/devel/unitree_ws/"
-    ROUT_GO2_MCP = "/root/devel/go2_mpc_ws/"
 
 
     def __init__(self):
         super().__init__()
 
     def displayCurrentEnv(self):
+        script_path = pathlib.Path(__file__).parent.resolve()
+        process_path = pathlib.Path().resolve()
+        print (f'Script in location {script_path}', file=sys.stderr)
+        print (f'Process in location {process_path}', file=sys.stderr)
 
-        dds_impl = os.environ['RMW_IMPLEMENTATION']
-        dds_uri = os.environ['CYCLONEDDS_URI']
-        ros_version = os.environ['ROS_DISTRO']
-        print ("\nCureent env:")
-        print (f'  - RMW_IMPLEMENTATION {dds_impl}')
-        print (f'  - CYCLONEDDS_URI {dds_uri}')
-        print (f'  - ROS_DISTRO {ros_version}')
+        dds_impl = os.environ.get('RMW_IMPLEMENTATION', None)
+        dds_uri = os.environ.get('CYCLONEDDS_URI', None)
+        ros_version = os.environ.get('ROS_DISTRO', None)
+        print ("\nCurrent env:", file=sys.stderr)
+        print (f'  - RMW_IMPLEMENTATION {dds_impl}', file=sys.stderr)
+        print (f'  - CYCLONEDDS_URI {dds_uri}', file=sys.stderr)
+        print (f'  - ROS_DISTRO {ros_version}', file=sys.stderr)
 
-        print ('\nPython packages')
+        print ('\nPython packages', file=sys.stderr)
         python_packages = ["unitree_go", "unitree_sdk2py", "pinocchio", "eigenpy", "proxsuite_nlp", "aligator", "simple_mpc"]
         for pypkg in python_packages:
             loader = importlib.util.find_spec(pypkg)
             found = loader is not None
-            print (f'  - {pypkg} is {found}')
+            print (f'  - {pypkg} is {found}', file=sys.stderr)
 
-        print ('\nPython paths')
-        python_paths = os.environ['PYTHONPATH'].split(":")
+        print ('\nPython paths', file=sys.stderr)
+        python_paths = os.environ.get('PYTHONPATH', "").split(":")
         for pypath in python_paths:
-            print (f'  - {pypath}')
+            print (f'  - {pypath}', file=sys.stderr)
 
     def generateEnv(self):
+        script_path = pathlib.Path(__file__).parent.resolve()
+        process_path = pathlib.Path().resolve()
+
         go2_net_info = Go2NetworkInfo()
         ifname = go2_net_info.getGo2InterfaceName()
 
-        print (f'\nSetup commands\n')
-        print (f'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp')
-        print (f'export CYCLONEDDS_URI=\'<CycloneDDS><Domain><General><Interfaces><NetworkInterface name="{ifname}" priority="default" multicast="default" /></Interfaces></General></Domain></CycloneDDS>\'')
-        print (f'source {self.ROOT_UNITREE}')
-        print (f'source {self.ROUT_GO2_MCP}')
+        print (f'\nSetup commands\n', file=sys.stderr)
+        print (f'source {script_path}/../../../install.bash;')
+        print (f'export CYCLONEDDS_HOME={script_path}/../../../cyclonedds/;')
+        print (f'export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp;')
+        print (f'export CYCLONEDDS_URI=\'<CycloneDDS><Domain><General><Interfaces><NetworkInterface name="{ifname}" priority="default" multicast="default" /></Interfaces></General></Domain></CycloneDDS>\';')
 
 def main(args = None):
     go2_env = Go2EnvSetup()
