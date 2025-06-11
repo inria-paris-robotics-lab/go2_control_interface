71c914b2740778ef315ea75ca18eebbbd9ad9e51
earlaud <etienne.arlaud@inria.fr>
2025-06-11 11:56:25 +0200

    Expose watchdog params in watchdog launchfile

--- launch/watchdog.launch.py
+++ launch/watchdog.launch.py
@@ -11,6 +11,16 @@ def generate_launch_description():
         default_value='False',
         description="If set to true, watchdog won't enforce joint position limits"
     )
+    freq_arg = DeclareLaunchArgument(
+        'freq',
+        default_value='100',
+        description="Polling frequency of the safety (freq * n_fails = timeout limit)"
+    )
+    n_fails_arg = DeclareLaunchArgument(
+        'n_fails',
+        default_value='2',
+        description="Number of polling step without receiving new joint command allowed (freq * n_fails = timeout limit)"
+    )
 
     watchdog_config_filepath = PathJoinSubstitution([
                                     FindPackageShare('go2_control_interface'),
@@ -20,11 +30,18 @@ def generate_launch_description():
 
     return LaunchDescription([
         ignore_joint_limits_arg,
+        freq_arg,
+        n_fails_arg,
         Node(
             package='go2_control_interface',
             executable='watchdog_node.py',
             name='watchdog',
             output='screen',
-            parameters=[watchdog_config_filepath, {"ignore_joint_limits": LaunchConfiguration("ignore_joint_limits")}],
+            parameters=[watchdog_config_filepath,
+            {
+             "ignore_joint_limits": LaunchConfiguration("ignore_joint_limits"),
+             "freq": LaunchConfiguration("freq"),
+             "n_fails": LaunchConfiguration("n_fails"),
+             }],
            ),
 ])
